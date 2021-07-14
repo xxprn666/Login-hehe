a19f3aa518a7face7e238844fda16d9a9e39c154
Dedekpradana <dedekpradanaa@gmail.com>
2021-07-15 00:59:56 +0800

    Update Heheh

--- myproject/forms.py
+++ myproject/forms.py
@@ -1,27 +1,42 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, TextField, IntegerField
 from wtforms.validators import DataRequired,Email,EqualTo
 from wtforms import ValidationError
+from myproject.models import User, Tempekan
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Log In')
+    submit = SubmitField('Login')
 
 
 class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(),Email()])
     username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired(), EqualTo('pass_confirm', message='Passwords Must Match!')])
+    password = PasswordField('Password', validators=[DataRequired(),
+    EqualTo('pass_confirm', message='Passwords Must Match!')])
     pass_confirm = PasswordField('Confirm password', validators=[DataRequired()])
     submit = SubmitField('Register!')
 
     def check_email(self, field):
-        # Check if not None for that user email!
         if User.query.filter_by(email=field.data).first():
             raise ValidationError('Your email has been registered already!')
 
     def check_username(self, field):
-        # Check if not None for that username!
         if User.query.filter_by(username=field.data).first():
             raise ValidationError('Sorry, that username is taken!')
+
+
+class TempekanForm(FlaskForm):
+    Tempekan = IntegerField('Tempekan', validators=[DataRequired()])
+    NamaLengkap = TextField('Nama Lengkap', validators=[DataRequired()])
+    NamaPangillan = TextField('Nama Pangillan', validators=[DataRequired()]
+    Tempat = TextField('Tempat lahir', validators=[DataRequired()])
+    TanggalLahir = IntegerField('Tanggal Lahir', validators=[DataRequired()])
+    Umur = IntegerField('Umur', validators=[DataRequired()])
+    Nik = IntegerField('NIK', validators=[DataRequired()])
+    AlamatLengkap = TextField('Alamat Lengkap', validators=[DataRequired()])
+    Email = StringField('Alamat Email', validators=[DataRequired()])
+    Bakat = TextField('Minat dan Bakat', validators=[DataRequired()])
+    NamaOrtu = TextField('Nama Orang tua', validators=[DataRequired()])
--- myproject/models.py
+++ myproject/models.py
@@ -11,8 +11,8 @@ class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key = True)
-    email = db.Column(db.String(64), unique=True, index=True)
-    username = db.Column(db.String(64), unique=True, index=True)
+    email = db.Column(db.String(64), unique = True, index = True)
+    username = db.Column(db.String(64), unique= True, index= True)
     password_hash = db.Column(db.String(128))
 
     def __init__(self, email, username, password):
@@ -22,3 +22,35 @@ class User(db.Model, UserMixin):
 
     def check_password(self,password):
         return check_password_hash(self.password_hash,password)
+
+class Tempekan(db.Model):
+
+    __tablename__ = 'Tempekan'
+
+    id = db.Column(db.Integer, primary_key = True)
+    Tempekan = db.Column(db.Integer)
+    NamaLengkap = db.Column(db.Text)
+    NamaPangillan = db.Column(db.Text)
+    TempatLahir = db.Column(db.Text)
+    TanggalLahir = db.Column(db.Integer)
+    Umur = db.Column(db.Integer)
+    Nik = db.Column(db.Integer)
+    Alamat = db.Column(db.Text)
+    NoHp = db.Column(db.Integer)
+    Email = db.Column(db.Text)
+    Bakat = db.Column(db.Text)
+    NamaOrtu = db.Column(db.Text)
+
+    def __init__(self, NamaLengkap, NamaPangillan, TanggalLahir, TempatLahir, Umur, Nik,
+            Alamat, NoHp, Email, Bakat, NamaOrtu):
+        self.NamaLengkap = NamaLengkap
+        self.NamaPangillan = NamaPangillan
+        self.TempatLahir = TempatLahir
+        self.TanggalLahir = TanggalLahir
+        self.Umur = Umur
+        self.Nik = Nik
+        self.Alamat = Alamat
+        self.NoHp = NoHp
+        self.Email = Email
+        self.Bakat = Bakat
+        self.NamaOrtu = NamaOrtu
